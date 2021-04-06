98997051ff3c8b0081d1895f079aa98bd14da2a7
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-04-06 08:53:36 +0600

    Firebase Connection

--- app/src/main/java/com/example/secondlesson/MainActivity.kt
+++ app/src/main/java/com/example/secondlesson/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.secondlesson
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.secondlesson.models.Dog
 import com.example.secondlesson.models.Tiger
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,41 +17,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val wildAnimal=findViewById(R.id.wild) as Button
-        val domesticAnimal=findViewById(R.id.domestic) as Button
+        val wildAnimal = findViewById(R.id.wild) as Button
+        val domesticAnimal = findViewById(R.id.domestic) as Button
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("animals")
+
 
 
         wildAnimal.setOnClickListener(View.OnClickListener { v: View? ->
 
-            val wild:Tiger= Tiger("Siberian");
+            val wild: Tiger = Tiger("Siberian");
+            myRef.setValue(wild)
 
 
-            val toast = Toast.makeText(applicationContext, wild.animalType, Toast.LENGTH_LONG)
-            toast.setGravity(Gravity.CENTER, 0, 0)
-
-            val toastContainer = toast.view as LinearLayout
-            val catImage = ImageView(this)
-            catImage.setImageResource(R.drawable.tiger)
-            toastContainer.addView(catImage, 0)
+            val toast = Toast(this)
+            val view = ImageView(this)
+            view.setImageResource(R.drawable.tiger)
+            toast.setView(view)
             toast.show()
+
+            Toast.makeText(this, "You have chosen " + wild.animalType, Toast.LENGTH_LONG).show()
 
         })
 
         domesticAnimal.setOnClickListener(View.OnClickListener { v: View? ->
-            val dog:Dog=Dog("Fluppy","Zheke")
+            val dog: Dog = Dog("Fluppy", "Zheke")
 
-            val toast = Toast.makeText(applicationContext, dog.petName, Toast.LENGTH_LONG)
-            toast.setGravity(Gravity.CENTER, 0, 0)
+            myRef.setValue(dog)
 
-            val toastContainer = toast.view as LinearLayout
-            val catImage = ImageView(this)
-            catImage.setImageResource(R.drawable.dog)
-            toastContainer.addView(catImage, 0)
+
+            val toast = Toast(this)
+            val view = ImageView(this)
+            view.setImageResource(R.drawable.dog)
+            toast.setView(view)
             toast.show()
+
+            Toast.makeText(this, "You have chosen " + dog.animalType, Toast.LENGTH_LONG).show()
         })
-
-
-
 
 
     }
